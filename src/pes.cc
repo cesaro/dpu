48de1fc011da55ae412965afbc210575aec03233
huyenntt <huyenntt.hnue@gmail.com>
2016-01-15 10:19:05 +0100

    connection test

--- src/pes.cc
+++ src/pes.cc
@@ -68,49 +68,43 @@ void Config::add(Event & e)
 
    switch (s->type)
       case ir::Trans::RD:
-      {
          this->latest_global_rdwr[s->proc][s->addr]=&e;
          break;
-      }
+
       case ir::Trans::WR:
-      {
        	 this->latest_global_wr[s->addr]=&e;
        	 this->latest_global_rdwr[s->proc][s->addr]=&e;
          break;
-      }
+
       case ir::Trans::SYN:
-      {
        	 this->latest_global_wr[s->addr]=&e;
        	 break;
-      }
+
    //update the event e
    e.pre_proc=latest_proc[s->proc];//e becomes the latest event of its process
    e.post_proc.clear(); // e has no children
    switch (s->type)
+   {
       case ir::Trans::RD:
-      {
-       	  e.pre_mem = latest_global_rdwr;
-       	  e.post_mem[e.val]=e.val;
-      }
+         e.pre_mem = latest_global_rdwr;
+       	 e.post_mem[e.val]=e.val;
+
       case ir::Trans::WR:
-      {
-      	  e.pre_mem=latest_global_wr;
-       	  std::vector<Event *>::iterator it;
-       	  for (it=e.post_mem.begin();it<e.post_mem.end();it++)
-          	  it=e.val;
-       	  // set pre-readers = set of latest events which use the variable in all processes.
-       	  //size of pre-readers is numbers of copies of the variable
-       	  for (it=e.pre_readers.begin();it<e.pre_readers.end();it++)
-       	  {
-       		  int numofproc=gs.m.procs.size();
-       		  for (i=0;i<numofproc;i++)
-      		  it=latest_global_rdwr[i][s->addr]; // the latest global event of the process
-       	  }
-       }
-       case ir::Trans::SYN:
-       case ir::Trans::LOC:
-
-
+         e.pre_mem=latest_global_wr;
+       	 std::vector<Event *>::iterator it;
+       	 for (it=e.post_mem.begin();it<e.post_mem.end();it++)
+            it=e.val;
+       	    // set pre-readers = set of latest events which use the variable in all processes.
+       	    //size of pre-readers is numbers of copies of the variable
+       	 for (it=e.pre_readers.begin();it<e.pre_readers.end();it++)
+       	 {
+       	    int numofproc=gs.m.procs.size();
+       		for (i=0;i<numofproc;i++)
+      		   it=latest_global_rdwr[i][s->addr]; // the latest global event of the process
+       	 }
+      case ir::Trans::SYN:
+      case ir::Trans::LOC:
+   }
 }
 
 } // end of namespace

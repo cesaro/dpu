2941870eb81418b490fbaaf57e0709aa33c7affa
Cesar Rodriguez <cesar.rodriguez@lipn.fr>
2016-11-21 18:23:53 +0100

    action.hpp

---
+++ src/action.hpp
@@ -0,0 +1,39 @@
+
+inline bool Action::operator== (const Action &other) const
+{
+   switch (type)
+   {
+   // loads, stores
+   case ActionType::RD8 :
+   case ActionType::RD16 :
+   case ActionType::RD32 :
+   case ActionType::RD64 :
+   case ActionType::WR8 :
+   case ActionType::WR16 :
+   case ActionType::WR32 :
+   case ActionType::WR64 :
+   // malloc
+   case ActionType::MALLOC :
+      return type == other.type and addr == other.addr and val == other.val;
+
+   // free and locks
+   case ActionType::FREE :
+   case ActionType::MTXLOCK :
+   case ActionType::MTXUNLK :
+      return type == other.type and addr == other.addr;
+
+   // threads
+   case ActionType::THCREAT :
+   case ActionType::THJOIN :
+      return type == other.type and val == other.val;
+
+   // threads
+   case ActionType::THSTART :
+   case ActionType::THEXIT :
+      return type == other.type;
+
+   default :
+      ASSERT (0);
+   }
+};
+

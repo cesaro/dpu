e410934dbbdb5ce54502a28dff92e436d64d8ef5
marcelosousa <dipython@gmail.com>
2017-02-17 06:18:18 -0500

    added two benchmarks

---
+++ benchmarks/spin17/ssbsimple.c
@@ -0,0 +1,57 @@
+#include <pthread.h>
+#include <assert.h>
+
+// This is an example of a program
+// with three threads that contains SSBs
+
+pthread_mutex_t mx;
+pthread_mutex_t my;
+pthread_mutex_t mz;
+int x = 0;
+
+void *ta(void *arg)
+{
+ pthread_mutex_lock(&mx);
+   x = 1;
+ pthread_mutex_unlock(&mx);
+
+ pthread_mutex_lock(&mz);
+ pthread_mutex_unlock(&mz);
+}
+
+void *tb(void *arg)
+{
+ pthread_mutex_lock (&mx);
+   if (x)
+   {
+     pthread_mutex_lock (&my);
+     pthread_mutex_unlock (&my);
+   }
+ pthread_mutex_unlock (&mx);
+}
+
+void *tc(void *arg)
+{
+ pthread_mutex_lock (&my);
+   pthread_mutex_lock (&mz);
+   pthread_mutex_unlock (&mz);
+ pthread_mutex_unlock (&my);
+}
+
+int main()
+{
+ pthread_t ida;
+ pthread_t idb;
+ pthread_t idc;
+ pthread_mutex_init(&mx, NULL);
+ pthread_mutex_init(&my, NULL);
+ pthread_mutex_init(&mz, NULL);
+
+ pthread_create(&idc,  NULL, tc, NULL);
+ pthread_create(&idb,  NULL, tb, NULL);
+ pthread_create(&ida,  NULL, ta, NULL);
+
+ pthread_join(ida,NULL);
+ pthread_join(idb,NULL);
+ pthread_join(idc,NULL);
+}

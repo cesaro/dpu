ccf9c857cdb196ed5cad291ce0cf09257a8487c6
huyenntt <huyenntt.hnue@gmail.com>
2017-05-25 01:24:16 +0200

    add benchmark svcomp17

---
+++ benchmarks/svcomp17/pthread/fib_bench_longer_true-unreach-call.c
@@ -0,0 +1,44 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+
+#include <pthread.h>
+
+int i=1, j=1;
+
+#define NUM 6
+
+void *
+t1(void* arg)
+{
+  int k = 0;
+
+  for (k = 0; k < NUM; k++)
+    i+=j;
+
+  pthread_exit(NULL);
+}
+
+void *
+t2(void* arg)
+{
+  int k = 0;
+
+  for (k = 0; k < NUM; k++)
+    j+=i;
+
+  pthread_exit(NULL);
+}
+
+int
+main(int argc, char **argv)
+{
+  pthread_t id1, id2;
+
+  pthread_create(&id1, NULL, t1, NULL);
+  pthread_create(&id2, NULL, t2, NULL);
+
+  if (i > 377 || j > 377) {
+    ERROR: __VERIFIER_error();
+  }
+
+  return 0;
+}
---
+++ benchmarks/svcomp17/pthread/sigma_false-unreach-call.c
@@ -0,0 +1,51 @@
+extern void __VERIFIER_assume(int);
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+void __VERIFIER_assert(int expression) { if (!expression) { ERROR: __VERIFIER_error();}; return; }
+
+const int SIGMA = 16;
+
+int *array;
+int array_index=-1;
+
+
+void *thread(void * arg)
+{
+	array[array_index] = 1;
+	return 0;
+}
+
+
+int main()
+{
+	int tid, sum;
+	pthread_t *t;
+
+	t = (pthread_t *)malloc(sizeof(pthread_t) * SIGMA);
+	array = (int *)malloc(sizeof(int) * SIGMA);
+
+	__VERIFIER_assume(t);
+	__VERIFIER_assume(array);
+
+	for (tid=0; tid<SIGMA; tid++) {
+	        array_index++;
+		pthread_create(&t[tid], 0, thread, 0);
+	}
+
+	for (tid=0; tid<SIGMA; tid++) {
+		pthread_join(t[tid], 0);
+	}
+
+	for (tid=sum=0; tid<SIGMA; tid++) {
+		sum += array[tid];
+	}
+
+	__VERIFIER_assert(sum == SIGMA);  // <-- wrong, different threads might use the same array offset when writing
+
+	return 0;
+}
+
